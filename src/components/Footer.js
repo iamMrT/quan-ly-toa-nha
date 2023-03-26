22a30950854fca4f705c8117770e98f93212952a
Nguyễn Quốc Tuấn <95756779+iamMrT@users.noreply.github.com>
2023-03-26 20:59:33 +0700

    init app

---
+++ src/components/Footer.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import '../css/footer.css'
+
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <div className="footer-content">
+                <div className="footer-item footer-contact">
+                    <div className="footer-item-title">Manage Company And Employees</div>
+                    <div className="footer-contact-container">
+                        <div className="footer-contact-item footer-contact-facebook">
+                            <div className="footer-contact-title">Facebook</div>
+                            <div className="footer-contact-content">Trần Minh Tuân</div>
+                        </div>
+                        <div className="footer-contact-item footer-contact-email">
+                            <div className="footer-contact-title">
+                                <i className='bx bxs-envelope footer-contact-icon'></i>
+                                Email
+                            </div>
+                            <div className="footer-contact-content">tranminhtuan110502@gmail.com</div>
+                        </div>
+                        <div className="footer-contac-item footer-contact-phone">
+                            <div className="footer-contact-title">
+                                <i className='bx bxs-phone footer-contact-icon'></i>
+                                Số điện thoại
+                            </div>
+                            <div className="footer-contact-content">
+                                <span>+84</span> 375511200
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-item footer-contact">
+                    <div className="footer-item-title">Manage Building Employee</div>
+                    <div className="footer-contact-container">
+                        <div className="footer-contact-item footer-contact-facebook">
+                            <div className="footer-contact-title">Facebook</div>
+                            <div className="footer-contact-content">Nguyễn Quốc Tuấn</div>
+                        </div>
+                        <div className="footer-contact-item footer-contact-email">
+                            <div className="footer-contact-title">
+                                <i className='bx bxs-envelope footer-contact-icon'></i>
+                                Email
+                            </div>
+                            <div className="footer-contact-content">quoctuan2001@gmail.com</div>
+                        </div>
+                        <div className="footer-contac-item footer-contact-phone">
+                            <div className="footer-contact-title">
+                                <i className='bx bxs-phone footer-contact-icon'></i>
+                                Số điện thoại
+                            </div>
+                            <div className="footer-contact-content">
+                                <span>+84</span> 375511200
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-item footer-contact">
+                    <div className="footer-item-title">Manage Floors And Contract </div>
+                    <div className="footer-contact-container">
+                        <div className="footer-contact-item footer-contact-facebook">
+                            <div className="footer-contact-title">Facebook</div>
+                            <div className="footer-contact-content">Nguyễn Văn Duy</div>
+                        </div>
+                        <div className="footer-contact-item footer-contact-email">
+                            <div className="footer-contact-title">
+                                <i className='bx bxs-envelope footer-contact-icon'></i>
+                                Email
+                            </div>
+                            <div className="footer-contact-content">nvd@gmail.com</div>
+                        </div>
+                        <div className="footer-contac-item footer-contact-phone">
+                            <div className="footer-contact-title">
+                                <i className='bx bxs-phone footer-contact-icon'></i>
+                                Số điện thoại
+                            </div>
+                            <div className="footer-contact-content">
+                                <span>+84</span> 375511200
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-item footer-contact">
+                    <div className="footer-item-title">Manage Registerd Services </div>
+                    <div className="footer-contact-container">
+                        <div className="footer-contact-item footer-contact-facebook">
+                            <div className="footer-contact-title">Facebook</div>
+                            <div className="footer-contact-content">Đỗ Quang Tâm</div>
+                        </div>
+                        <div className="footer-contact-item footer-contact-email">
+                            <div className="footer-contact-title">
+                                <i className='bx bxs-envelope footer-contact-icon'></i>
+                                Email
+                            </div>
+                            <div className="footer-contact-content">quangtuam@gmail.com</div>
+                        </div>
+                        <div className="footer-contac-item footer-contact-phone">
+                            <div className="footer-contact-title">
+                                <i className='bx bxs-phone footer-contact-icon'></i>
+                                Số điện thoại
+                            </div>
+                            <div className="footer-contact-content">
+                                <span>+84</span> 375511200
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
